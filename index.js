d21fc84db01915f710fd585fc5af4c9279caa287
Joshua <joshaw66@gmail.com>
2020-01-15 19:26:36 -0700

    finished first 5

--- index.js
+++ index.js
@@ -84,8 +84,12 @@ const person = {
  *         and returns a string like `Hello, my name is {name}`.
  *         where `{name}` is the name passed into `makeSmartPerson`.
 */
-function makeSmartPerson(/* code here */) {
-  /* code here */
+function makeSmartPerson(name) {
+  return{
+    name: 'Sam',
+    sum: (num1, num2) => (num1 + num2),
+    speak: () => 'Hello, my name is ${name}'
+  }
 }
 
 
