8783082f2da6f8d4a3bd0b1dbeaff21347d75187
Joshua <joshaw66@gmail.com>
2020-01-16 21:04:27 -0700

    solved the convert to arrow functions. WHY COULDN'T THEY ALL BE THIS EASY

--- index.js
+++ index.js
@@ -285,9 +285,9 @@ function getGermanCars(cars_inventory) {
  *   return num * 2
  * }
 */
-const sum = null; // code here!
-const addFive = null; // code here!
-const argTimesTwo = null; // code here!
+const sum = (a, b) => (a + b);
+const addFive =  num => (num + 5);
+const argTimesTwo = num => (num * 2);
 
 /**
  * ### Challenge `carMaker`
